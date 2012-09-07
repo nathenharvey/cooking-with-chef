de62d0adecaea79f4cff2541412d0a1da2072b62
Nathen Harvey <nharvey@customink.com>
2012-09-07 16:39:13 -0400

    don't need the bundler recipe

--- chef-repo/cookbooks/dcrug/recipes/web.rb
+++ chef-repo/cookbooks/dcrug/recipes/web.rb
@@ -16,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe "bundler"
 
 deploy_to = "/u/apps"
 app_user = "www-data"
